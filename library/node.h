64db1d17de000ca537ebbe37394eb36a2285cf8f
IgorKruchinin <93050201+IgorKruchinin@users.noreply.github.com>
2024-12-06 17:50:38 +0300

    Update node.h

--- library/node.h
+++ library/node.h
@@ -4,11 +4,11 @@ template <typename T>
 struct Node {
     T value;
     Node *next;
-    Node(int value, Node* next = nullptr);
+    Node(T value, Node* next = nullptr);
 };
 
 template <typename T>
-Node<T>::Node(int value, Node<T>* next) {
+Node<T>::Node(T value, Node<T>* next) {
         this->value = value;
         this->next = next;
 }
